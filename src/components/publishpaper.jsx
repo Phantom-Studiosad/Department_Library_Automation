ca7f16ca66ea6581f9892ec77aa00c93a6abe05b
Dharun Naraynan <lkdharun@gmail.com>
2022-04-18 23:08:25 +0530

    publish_paper

--- src/components/publishpaper.jsx
+++ src/components/publishpaper.jsx
@@ -35,6 +35,8 @@ function PublishPaper(){
         behavior: 'smooth' // for smoothly scrolling
       });
     };
+
+    
     return (
         <div>
           {
@@ -79,10 +81,10 @@ function PublishPaper(){
                         </div>  
                     </div>
                     <div class="container">
-                        <div class="container-fluid" id="grad1">
+                        <div class="containerr-fluid" id="grad1">
                             <div class="row justify-content-center mt-0">
-                                <div class="col-11 col-sm-9 col-md-7 col-lg-6 text-center p-0 mt-3 mb-2">
-                                    <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
+                                <div class="text-center">
+                                    <div class="rcard px-0 pt-4 pb-0 mt-3 mb-3">
                                         <h2><strong>Add details and upload your paper</strong></h2>
                                         <p>Fill all form field to go to next step</p>
                                         <div class="row">
@@ -97,18 +99,8 @@ function PublishPaper(){
                                                     <fieldset>
                                                         <div class="form-card">
                                                             <div class="form-group" style={{fontSize:'20px'}}>
-                                                                <label class="form-control-label" for="title">Title</label>
-                            
-                                                                <input
-                                                                    class="form-control"
-                                                                    id="title"
-                                                                    maxlength="255"
-                                                                    minlength="1"
-                                                                    name="title"
-                                                                    required
-                                                                    type="text"
-                                                                    value=""
-                                                                />
+                                                                <label class="form-control-label" for="title">Title</label>                           
+                                                                <input class="form-control" id="title" maxlength="255" minlength="1" name="title" required type="text" ></input>
                                                             </div>
                                                             <div class="form-group" style={{fontSize:'20px'}}>
                                                                 <label class="form-control-label" for="paper_type">
@@ -150,7 +142,94 @@ function PublishPaper(){
                                                                 </div>
                                                             </div>
                                                         </div>     
-                                                        <input type="button" name="next" class="next action-button" value="Next Step" />                                                   
+                                                        <div class="form-card">
+                                                            <h2 class="fs-title">Personal Information</h2> 
+                                                            <div>
+                                                            <label class="form-control-label" for="department_area" style={{fontSize:'20px'}}>Department Area Collection</label>
+                                                            <select class="form-control dept-area-grp" id="department_area" name="department_area" required="" style={{width:'20%',height:'75%', border:'1px solid black',padding:'6px 6px'}}>
+                                                            <optgroup label="Computer Science and Engineering">
+                                                                <option value="1">Artificial Intelligence</option>
+
+                                                                <option value="2">Machine Learning</option>
+
+                                                                <option value="3">Neural Networks</option>
+
+                                                                <option value="4">Cloud Computing</option>
+                                                                
+                                                                <option value="5">Internet Of Things</option>
+                                                            </optgroup>
+
+                                                            <optgroup
+                                                                label="Electronics and Communication Engineering"
+                                                            ></optgroup>
+
+                                                            <optgroup label="Mechanical Engineering"></optgroup>
+                                                            </select>
+
+                                                            </div>
+                                                            <div class="form-group" style={{fontSize:'20px'}}>
+                                                                <label class="form-control-label" for="authors">Author</label>
+                            
+                                                                <input
+                                                                    class="form-control"
+                                                                    id="author"
+                                                                    maxlength="255"
+                                                                    minlength="1"
+                                                                    name="author"
+                                                                    required
+                                                                    type="text"
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-card">
+                                                            <div class="form-group" style={{fontSize:'20px'}}>
+                                                                <label class="form-control-label" for="publisher">Publisher</label>
+                                                
+                                                                <input
+                                                                class="form-control"
+                                                                id="publisher"
+                                                                maxlength="255"
+                                                                minlength="1"
+                                                                name="publisher"
+                                                                required
+                                                                type="text"
+                                                                />
+                                                            </div>
+                                                
+                                                            <div class="form-group" style={{fontSize:'20px'}}>
+                                                                <label class="form-control-label" for="published_year">Publishing Year</label>
+                                                
+                                                                <input
+                                                                class="form-control"
+                                                                id="published_year"
+                                                                max="2022"
+                                                                name="published_year"
+                                                                required
+                                                                type="number"
+                                                                />
+                                                            </div>
+                                                        </div> 
+                                                        <div class="form-card">
+                                                            <div class="form-group" style={{fontSize:'20px'}}>
+                                                                <label class="form-control-label" for="abstract">Abstract</label>
+                                                                <textarea type="text" id="abstract" name="abstract"  class="form-control " required></textarea>                                                
+                                                            </div>
+                                                
+                                                            <div class="form-group" style={{fontSize:'20px'}}>
+                                                                <label class="form-control-label" for="paper_file">Upload file as pdf</label>
+                                                                <input
+                                                                class="form-control"
+                                                                id="paper_file"
+                                                                name="paper_file"
+                                                                type="file"
+                                                                style={{backgroundColor:'transparent',border:'none'}}
+                                                                />
+                                                            </div>
+                                                
+                                                        </div>
+                                                        <button id="submit" type="submit" class="btn btn-success" style={{fontSize:'18px'}}>
+                                                                    <i class="fa fa-upload"></i> Publish Paper
+                                                        </button>                                                 
                                                     </fieldset>
                                                 </form>
                                             </div>
